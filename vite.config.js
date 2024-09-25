a938a62b3cdec499033363b7b6841cd0bf8fe204
Hola <denishola03@gmail.com>
2024-09-25 14:08:35 -0300

    1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   return {
+    base: 'https://github.com/Denis3206/Sistema---DATA', // Cambia esto a la ruta de tu proyecto
     define: {
       'process.env': env,
     },
